b2c451ec3fc9f7cc0627cc5e3fb660de95177444
anton <darvin199611@gmail.com>
2017-03-24 17:39:08 +0200

    лютый пиздец

---
+++ uchet/static/js/login_check.js
@@ -0,0 +1,24 @@
+function checkLogin(login){
+
+    if(login!=0) {
+        $.ajax({
+            type: "GET",
+            url: "/check_login/",
+            data: {username: login,},
+            dataType: "text",
+            cache: false,
+            success: function (data) {
+                console.log(data);
+                if (data == 'yes') {
+                    console.log("yes");
+                }
+                else if (data == 'no') {
+                    console.log("no");
+                }
+            }
+
+
+        });
+    }
+
+}
