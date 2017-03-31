7bce491b2320fabb19773a2f6357ac09b6f733a3
Wella <wellaxp@gmail.com>
2017-03-31 14:09:37 +0300

    Профиль

--- uchet/static/js/sale.js
+++ uchet/static/js/sale.js
@@ -5,12 +5,11 @@
         }).done(function(data) {
             console.log(data);
             var table = $('#sales');
-            table.html('<tr> <th>id товара</th> <th>id магазина</th> <th>название товара</th> <th>date</th> </tr>');
+            table.html('<tr> <th>id товара</th><th>название товара</th> <th>date</th> </tr>');
             data.submissions.forEach(function(sale) {
                 console.log(sale);
                 var row = $('<tr>');
                 row.append($('<td>').text(sale.id));
-                row.append($('<td>').text(sale.market));
                 row.append($('<td>').text(sale.stuff_name));
                 row.append($('<td>').text(" tyt budet vremya"));
                 table.prepend(row);
