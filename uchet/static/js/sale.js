d4ef89ed96ae74d8f5d3222d1b8e383de5309e52
anton <darvin199611@gmail.com>
2017-03-31 13:57:59 +0300

    добавил вывод сегодняшних продаж

--- uchet/static/js/sale.js
+++ uchet/static/js/sale.js
@@ -1,9 +1,7 @@
-(function() {
     function loadSubmissions() {
         $.get('/get_market_sales', {
             'market_id': $('#market_id').val()
         }).done(function(data) {
-            console.log(data);
             var table = $('#sales');
             table.html('<tr><th>наименование товара</th><th>Цена</th><th>Время</th> </tr>');
             data.submissions.forEach(function(sale) {
@@ -18,6 +16,5 @@
     }
 
     loadSubmissions();
-
     // setInterval(loadSubmissions, 5000);
-})();
+
