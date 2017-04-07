4050c27ddb5dcfca86f4fec1d17d26a38831ff29
anton <darvin199611@gmail.com>
2017-04-08 01:38:49 +0300

    страница управления магазином , недоделаная таблица продаж

--- uchet/views.py
+++ uchet/views.py
@@ -245,13 +245,20 @@ def store_control(request, id):
     market = Market.objects.get(id=id)
     check_premissions(request, market)  # функция проверяет принадлежит ли запрашиваемый экземпляр пользователю ,
                                         # если нет , возвращает 404
+    sales = Sale.objects.filter(stuff__market_id=market_id)
+    print(sales)
     stuffs = Stuff.objects.filter(market_id=market_id).order_by('created_date')
     stuff = []
+    sale = []
+    for obj in sales:
+        sale_list = {'stuff_name': obj.stuff,
+                     'date': (timezone.localtime(obj.datetime)).strftime('%Y-%m-%d %H:%M')}
+        sale.append(sale_list)
     for obj in stuffs:
         stuffss = {'id': obj.id, 'name': obj.name, 'picture': obj.picture,
                    'price': str(obj.price), 'amount': str(obj.amount),
                    'date': (timezone.localtime(obj.created_date)).strftime('%Y-%m-%d %H:%M')}
         stuff.append(stuffss)
     market_choose = True
-    return render(request, 'uchet/control.html', {'market_choose': market_choose, 'stuffs': stuff})
+    return render(request, 'uchet/control.html', {'market_choose': market_choose, 'stuffs': stuff, 'sales': sale, })
 
